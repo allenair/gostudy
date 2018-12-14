dbd8fceb7891eefa8118767104bad0d0604d7702
allenair <allenair@126.com>
2018-12-14 13:40:10 +0800

    20181213

--- mytool/demo1213.go
+++ mytool/demo1213.go
@@ -12,4 +12,19 @@ func Main1213() {
 	str := "12.ghhhjl哈哈哈;1、个哼哼3.2m唧唧；23.fghjk;34.34chjk"
 
 	fmt.Println(r.FindAllString(str, -1))
+
+	indexList := make([]int,0)
+
+	for _, data := range r.FindAllStringIndex(str, -1) {
+		indexList=append(indexList, data[0])
+	}
+	indexList = append(indexList, len(str))
+
+	
+
+	for i:=0; i<len(indexList)-1;i++{
+		fmt.Println(str[indexList[i]:indexList[i+1]])
+	}
+
+	
 }
